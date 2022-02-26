63af459c3284389b1b264bee5643db6f25cbb49f
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-02-26 15:10:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+<br />
+
+<div align="center">
+   <img style="margin: 0 auto; padding-bottom: 15px; padding-top: 30px" width=70%" src="https://firebasestorage.googleapis.com/v0/b/blankly-6ada5.appspot.com/o/blankly-github-logo.png?alt=media&token=8f436cd2-3d28-432c-867a-afef780f4260">
+</div>
+<br />
+<div align="center">
+  <b>💨  View, manage and share your model from any codebase with slate  🚀</b>
+</div>
+<br />
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, './README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='blankly-slate',
+    packages=find_packages(),
+    version='v1.0.9-beta',
+    license='mit',
+    description='View, manage and share your model from any codebase with slate',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='blankly',
+    url='https://blankly.finance',
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        # Possible: "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+)
