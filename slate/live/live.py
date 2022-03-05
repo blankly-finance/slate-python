f9322651fb1aed0f3051df3d70f477fc247ba8fd
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-03-05 09:48:01 -0500

    Finish live routes

--- slate/live/live.py
+++ slate/live/live.py
@@ -1,3 +1,6 @@
+import json
+import warnings
+
 from slate.api import API
 
 
@@ -30,10 +33,160 @@ class Live:
         """
         return self.__live_base + route
 
-    def event(self, args: dict, response: dict, type_: str, annotation: str = None):
+    def event(self, args: dict, response: dict, type_: str, annotation: str = None) -> dict:
+        """
+        Post an event to the platform - generally used for annotating & viewing any important custom function calls
+        https://docs.blankly.finance/services/events/#post-v1liveupdate-trade
+
+        :param args: The (function) arguments
+        :param response: The (function) response
+        :param type_: A custom type for the event like 'order' or 'check_price'
+        :param annotation: A human-friendly bit of text for your function
+        :return: API Response
+        """
         return self.__api.post(self.__assemble_live_base('/event'), {
             'args': args,
             'response': response,
             'type': type_,
             'annotation': annotation
         })
+
+    def spot_market(self, symbol: str, id_: str, side: str, exchange: str, size: [float, int] = None,
+                    funds: [float, int] = None, annotation: str = None) -> dict:
+        """
+        Post a market order done on a spot exchange to the platform
+        https://docs.blankly.finance/services/events/#post-v1liveupdate-trade
+
+        :param symbol: The symbol the order is under
+        :param id_: The exchange-given order id
+        :param side: The side - 'buy' or 'sell'
+        :param exchange: The exchange the order was performed on - ex: 'coinbase_pro' or 'alpaca'
+        :param size: Mutually exclusive with funds - if you place an order priced in the base asset
+        :param funds: Mutually exclusive with size - if you place an order priced in the quote asset
+        :param annotation: An optional annotation to be given to this order
+        :return: API response (dict)
+        """
+        return self.__api.post(self.__assemble_live_base('/spot-market'), {
+            'symbol': symbol,
+            'id': id_,
+            'side': side,
+            'exchange': exchange,
+            'size': size,
+            'funds': funds,
+            'annotation': annotation
+        })
+
+    def spot_limit(self, symbol: str, id_: str, side: str, exchange: str, price: [int, float],
+                   size: [float, int] = None, funds: [float, int] = None, annotation: str = None):
+        """
+        Post a limit order done on a spot exchange to the platform
+        https://docs.blankly.finance/services/events/#post-v1liveupdate-trade
+
+        :param symbol: The symbol the order was placed on
+        :param id_: The exchange-generated order id
+        :param side: The order side - 'buy' or 'sell'
+        :param price: The limit order price
+        :param exchange: The exchange the order was performed on - ex: 'coinbase_pro' or 'alpaca'
+        :param size: Mutually exclusive with funds - if you place an order priced in the base asset
+        :param funds: Mutually exclusive with size - if you place an order priced in the quote asset
+        :param annotation: An optional annotation to be given to this order
+        :return: API response (dict)
+        """
+        return self.__api.post(self.__assemble_live_base('/spot-limit'), {
+            'symbol': symbol,
+            'id': id_,
+            'side': side,
+            'exchange': exchange,
+            'price': price,
+            'size': size,
+            'funds': funds,
+            'annotation': annotation
+        })
+
+    def spot_stop(self, symbol: str, id_: str, side: str, exchange: str, price: [int, float], activate: [int, float],
+                  size: [float, int] = None, funds: [float, int] = None, annotation: str = None):
+        """
+        Post a limit order done on a spot exchange to the platform
+        https://docs.blankly.finance/services/events/#post-v1liveupdate-trade
+
+        :param symbol: The symbol the order was placed on
+        :param id_: The exchange-generated order id
+        :param side: The order side - 'buy' or 'sell'
+        :param price: The limit order price
+        :param activate: The activation price of the stop order
+        :param exchange: The exchange the order was performed on - ex: 'coinbase_pro' or 'alpaca'
+        :param size: Mutually exclusive with funds - if you place an order priced in the base asset
+        :param funds: Mutually exclusive with size - if you place an order priced in the quote asset
+        :param annotation: An optional annotation to be given to this order
+        :return: API response (dict)
+        """
+        return self.__api.post(self.__assemble_live_base('/spot-limit'), {
+            'symbol': symbol,
+            'id': id_,
+            'side': side,
+            'exchange': exchange,
+            'price': price,
+            'activate': activate,
+            'size': size,
+            'funds': funds,
+            'annotation': annotation,
+        })
+
+    def update_trade(self, id_: str, **kwargs) -> dict:
+        """
+        Update an existing trade on the platform by order ID
+        https://docs.blankly.finance/services/events/#post-v1liveupdate-trade
+
+        :param id_: The exchange-given order id
+        :param kwargs: Any key/value pair. Generally these should be the same as the above order keys
+        :return: API Response (dict)
+        """
+        kwargs['id'] = id_
+        return self.__api.post(self.__assemble_live_base('/update-trade'), kwargs)
+
+    def update_annotation(self, id_: str, annotation: str) -> dict:
+        """
+        Explicitly update just the order annotation
+        https://docs.blankly.finance/services/events/#post-v1liveupdate-annotation
+
+        :param id_: The exchange-given order id
+        :param annotation: A descriptor about the order
+        :return: API Response (dict)
+        """
+        return self.__api.post(self.__assemble_live_base('/update-annotation'), {
+            'id': id_,
+            'annotation': annotation
+        })
+
+    def screener_result(self, result: dict) -> dict:
+        """
+        Post a screener result to the platform
+        https://docs.blankly.finance/services/events/#post-v1livescreener-result
+
+        :param result: The screener results object
+            example:
+            {
+                "AAPL": {              // Organize by symbol
+                    "RSI": 29.5,       // Keep the same keys internally across all symbols
+                    "buy_signal": true // Keep same keys
+                },
+                "MSFT": {
+                    "RSI": 54.3,       // Nesting beyond this level is not supported
+                    "buy_signal": false
+                }
+            }
+        :return: API response (dict)
+        """
+
+        # Test for double nesting in the results dictionary
+        for i in result:
+            inner = result[i]
+            for j in inner:
+                if isinstance(inner[j], dict):
+                    warnings.warn(f"Double nested dictionaries are not supported in the screener result: "
+                                  f"\n{json.dumps(inner[j], indent=2)}")
+
+        # Format to API spec
+        result = {'result': result}
+
+        return self.__api.post(self.__assemble_live_base('/screener-result'), result)
